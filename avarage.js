b835c65835282a10db87340c8c35483307642745
den-m1985 <denismartyanov244@gmail.com>
2022-09-05 15:09:23 +0200

    Создал сумму массива

---
+++ avarage.js
@@ -0,0 +1,11 @@
+a = [2,6,8,10,99,22568];
+size = a.length;
+index = 0;
+sum = 0;
+ while (index < size) {
+    sum = sum + a[index]
+    index = index + 1;
+ }
+ console.log(sum);
+
+//  Находим сумму массива
