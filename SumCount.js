dc4719a8245d00ee6874c92de5e675583122966c
den-m1985 <denismartyanov244@gmail.com>
2022-09-05 15:15:02 +0200

    Создал script кол-ва чисел

---
+++ SumCount.js
@@ -0,0 +1,12 @@
+a = [2,6,8,10,99,22568];
+size = a.length;
+index = 0;
+counts = 0;
+ while (index < size) {
+    counts = counts + 1
+    index = index + 1;
+ }
+ console.log(counts);
+
+
+//  Нашли кол-во чисел
