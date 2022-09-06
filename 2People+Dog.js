5df5fe7cb1d8d85d85fac3603202c3545f4a9e0a
den-m1985 <denismartyanov244@gmail.com>
2022-09-06 14:57:38 +0200

    Изменения от 6.09.22

---
+++ 2People+Dog.js
@@ -0,0 +1,22 @@
+Ferst_Friend_Speed = 1;
+Second_Friend_Speed = 2;
+Friend = 2;
+Dog_Speed = 3;
+length = 10000;
+count_dog = 0;
+time = 0;
+       while(length<10){
+         if (Friend = 1)
+         time = length / (Second_Friend_Speed + Dog_Speed);
+         Friend = 2;
+         if (friend = 2)
+         time = length / (Ferst_Friend_Speed + Dog_Speed)
+         Friend = 1;   
+         }
+        length = length - (Ferst_Friend_Speed + Second_Friend_Speed) * time
+        count_dog++ 
+console.log(count_dog);
+
+// Задачка с 1 лекции "Введение в программирование" 2 человека идут навстречу друг другу и 
+// от первого бежит собока к 2му человеку. Нужно найти сколько раз собака сделает пробежек.
+// На растоянии 10 между людьми, цикл должен остановится.
