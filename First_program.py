2140419384e3c9afb8df1e1a37b9cfa942d640d5
den-m1985 <denismartyanov244@gmail.com>
2022-09-09 16:31:34 +0200

    Добавил програмку в питоне с практикума

---
+++ First_program.py
@@ -0,0 +1,16 @@
+
+a=1452
+b=-10
+c=25645245
+if a > b:
+    if a > c:
+        print (a)
+    else:
+        print (c)
+elif b > c:
+    print (b)
+else:
+    print (c)
+    
+"Задачка нахождения максимального числа"
+"elif Эта та же функция что и else и if только вместе"
